2c671326bbe703d8f673a31d5b61bb32d53102f5
Daniel Barton <mineminecraftpe687@gmail.com>
2022-12-12 20:24:11 +1100

    Push from Github Desktop

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
+<img src="icons/icon128.png" align="right">
+
 # discordtokenlogin
- allows you to login to discord with ur token
+Allows you to login to discord with a token
+
+## How to use
+Download the zip file and import the extension into your browser.
+
+Then to login with a token, go to <a href="https://discord.com/login#tokenlogin" target="_blank">discord.com/login#tokenlogin</a> and enter the token when prompted.
+
+-------
+made with ❤️ by [daniel4-scratch](https://github.com/danie4-scratch)
---
+++ content.js
@@ -0,0 +1,24 @@
+// Author: @Daniel4-scratch
+
+var pageHash = window.location.hash;
+pageHash = pageHash.replace("#", "");
+
+function login(token) {
+    setInterval(() => {
+        document.body.appendChild(document.createElement`iframe`).contentWindow.localStorage.token = `"${token}"`
+    }, 50);
+    setTimeout(() => {
+        location.reload();
+    }, 2500);
+}
+
+if (pageHash == "tokenlogin"){
+    try{
+    var token = prompt("Enter your token");
+    login(token);
+    // window.location.href = "https://discord.com/app"; not reqiuired
+    }catch(err){
+        alert("An error occured during the login process. Please try again.");
+        console.alert("Discord Token Login Error: " + err);
+    }
+}
